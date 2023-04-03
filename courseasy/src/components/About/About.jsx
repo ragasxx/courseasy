cd15dfed5d49c1fafe2da26c4529e6ff8091b2e9
Sagar Bhatia <sagarbhatia722@gmail.com>
2023-04-04 00:13:45 +0530

    about page

--- courseasy/src/components/About/About.jsx
+++ courseasy/src/components/About/About.jsx
@@ -1,7 +1,41 @@
+import {
+  Avatar,
+  Container,
+  Heading,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 import React from 'react';
 
+const Founder = () => (
+  <Stack direction={['column', 'row']} spacing={['4', '16']} padding="8">
+    <VStack>
+      <Avatar
+        src="https://wallpaperaccess.com/full/551959.jpg"
+        boxSize={['40', '48']}
+      />
+      <Text children="Co-Founder" opacity={0.7} />
+    </VStack>
+    <VStack justifyContent={'center'} alignItems={['center', 'flex-start']}>
+      <Heading children="Kaito" size={['md', 'xl']} />
+      <Text
+        children={`Hi, I am a full-stack developer and a teacher.Our mission is to 
+    quality content at reasonable price.`}
+        textAlign={['center', 'left']}
+      />
+    </VStack>
+  </Stack>
+);
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <Container maxW={'container.lg'} padding="16" boxShadow={'lg'}>
+      <Heading children="About Us" textAlign={['center', 'left']}></Heading>
+      <Founder />
+      <Stack m='8' direction={['column','row']} alignItems='center'></Stack>
+    </Container>
+  );
 };
 
 export default About;
