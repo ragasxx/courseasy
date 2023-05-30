dedb437081581ed1d04a823ce4f9b86546217bff
Sagar Bhatia <sagarbhatia722@gmail.com>
2023-05-30 14:00:39 +0530

    cors

--- courseasy/src/redux/Store.js
+++ courseasy/src/redux/Store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userReducer } from './reducers/UserReducer';
 
-export const server = 'http://localhost:4000/api/v1';
+export const server = 'https://courseasy-server.vercel.app/api/v1';
 
 const store = configureStore({
   reducer: {
--- courseasyserver/app.js
+++ courseasyserver/app.js
@@ -16,9 +16,15 @@ app.use(
     extended: true,
   })
 );
-
 // to access cookies we need cookie parser
 app.use(cookieParser());
+
+app.use(cors({
+  origin:process.env.FRONTEND_URL,
+  credentials:true,
+  methods:["GET","POST","PUT","DELETE"]
+}))
+
 
 // importing and using routes
 import course from "./routes/courseRoutes.js";
