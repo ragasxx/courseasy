1093e7f434a113f57600c533ed4c8af7f5f5c731
Sagar Bhatia <sagarbhatia722@gmail.com>
2023-06-14 17:01:28 +0530

    get users completed

--- courseasy/src/components/Admin/Users/Users.jsx
+++ courseasy/src/components/Admin/Users/Users.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Box,
   Button,
@@ -14,75 +14,70 @@ import {
   Tr,
 } from '@chakra-ui/react';
 import Sidebar from '../Sidebar';
-import { RiDeleteBin3Fill, RiDeleteBin7Fill } from 'react-icons/ri';
+import { RiDeleteBin7Fill } from 'react-icons/ri';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteUser, getAllUsers, updateUser } from '../../../redux/actions/admin';
+import Loader from "../../Layout/Loader/Loader.jsx"
+import {toast} from "react-hot-toast";
+
+
 const Users = () => {
-  const users = [
-    {
-      _id: '1122323223',
-      name: 'sagar',
-      email: 'sagar@gmail.com',
-      role: 'user',
-      subscription: {
-        status: 'active',
-      },
-    },
-    {
-      _id: '112233223',
-      name: 'sagar1',
-      email: 'sagar@gmail.com',
-      role: 'user',
-      subscription: {
-        status: 'active',
-      },
-    },
-    {
-      _id: '112232322',
-      name: 'sagar2',
-      email: 'sagar@gmail.com',
-      role: 'user',
-      subscription: {
-        status: 'active',
-      },
-    },
-  ];
+
+  const {users,loading,error,message} = useSelector(state=>state.admin)
+  const dispatch = useDispatch();
 
   const updateHandler = userId => {
-    console.log(userId);
+   dispatch(updateUser(userId))
   };
 
   const deleteButtonHandler = userId => {
-    console.log(userId);
+   dispatch(deleteUser(userId))
   };
+
+
+  useEffect(() => {
+    dispatch(getAllUsers())
+    if(error){
+      toast.error(error);
+      dispatch({type:"clearError"});
+    }
+    if(message){
+      toast.success(message);
+      dispatch({type:"clearMessage"});
+    }
+  }, [dispatch,error,message])
 
   return (
     <Grid minH={'100vh'} templateColumns={['1fr', '5fr 1fr']}>
-      <Box p={['0', '16']} overflowX={'auto'}>
-        <Heading
-          children="All users"
-          size={'xl'}
-          textTransform={'uppercase'}
-          textAlign={['center', 'left']}
-          my={'8'}
-        />
+     {loading?<Loader />:(
+       <Box p={['0', '16']} overflowX={'auto'}>
+       <Heading
+         children="All users"
+         size={'xl'}
+         textTransform={'uppercase'}
+         textAlign={['center', 'left']}
+         my={'8'}
+       />
 
-        <TableContainer w={['100vw', 'full']}>
-          <Table variant={'simple'} size={'lg'}>
-            <Thead>
-              <Tr>
-                <Th>Id</Th>
-                <Th>Name</Th>
-                <Th>Email</Th>
-                <Th>Role</Th>
-                <Th>Subscription</Th>
-                <Th isNumeric>Action</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {users && users.map(item => <Row key={item._id} item={item} updateHandler={updateHandler} deleteButtonHandler={deleteButtonHandler} />)}
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Box>
+       <TableContainer w={['100vw', 'full']}>
+         <Table variant={'simple'} size={'lg'}>
+           <Thead>
+             <Tr>
+               <Th>Id</Th>
+               <Th>Name</Th>
+               <Th>Email</Th>
+               <Th>Role</Th>
+               <Th>Subscription</Th>
+               <Th isNumeric>Action</Th>
+             </Tr>
+           </Thead>
+           <Tbody>
+             {users && users.map(item => <Row key={item._id} item={item} updateHandler={updateHandler} deleteButtonHandler={deleteButtonHandler} />)}
+           </Tbody>
+         </Table>
+       </TableContainer>
+     </Box>
+     )}
       <Sidebar />
     </Grid>
   );
@@ -97,7 +92,7 @@ function Row({ item, updateHandler, deleteButtonHandler }) {
       <Td>{item.name}</Td>
       <Td>{item.email}</Td>
       <Td>{item.role}</Td>
-      <Td>{item.subscription.status === 'active' ? 'Active' : 'Not Active'}</Td>
+      <Td>{item.subscription&&item.subscription.status === 'active' ? 'Active' : 'Not Active'}</Td>
       <Td isNumeric>
         <HStack justifyContent={'flex-end'}>
           <Button
--- courseasy/src/redux/actions/admin.js
+++ courseasy/src/redux/actions/admin.js
@@ -64,3 +64,49 @@ export const deleteLecture = (courseId,lectureId)=> async(dispatch)=>{
         dispatch({type:"deleteLectureFail",payload:error.response.data.message})
     }
 }
+
+export const getAllUsers = ()=> async(dispatch)=>{
+    
+    try {
+        dispatch({type:"getAllUsersRequest"})
+        const{data} = await axios.get(`${server}/admin/users`,{
+            headers:{
+                "Content-Type":"application/json"
+            },
+            withCredentials:true, 
+        });
+        dispatch({type:"getAllUsersSuccess",payload:data.users})
+
+    } catch (error) { 
+        dispatch({type:"getAllUsersFail",payload:error.response.data.message})
+    }
+}
+
+export const deleteUser = (id)=> async(dispatch)=>{
+    try {
+        dispatch({type:"deleteUserRequest"})
+        const{data} = await axios.delete(`${server}/admin/user/${id}`,{
+            withCredentials:true, 
+        });
+        dispatch({type:"deleteUserSuccess",payload:data.message})
+
+    } catch (error) { 
+        dispatch({type:"deleteUserFail",payload:error.response.data.message})
+    }
+}
+
+export const  updateUser = (id)=> async(dispatch)=>{
+    try {
+        dispatch({type:"updateUserRoleRequest"})
+        const{data} = await axios.put(`${server}/admin/user/${id}`,{},{
+            headers:{
+                "Content-Type":"application/json"
+            },
+            withCredentials:true, 
+        });
+        dispatch({type:"updateUserRoleSuccess",payload:data.message})
+
+    } catch (error) { 
+        dispatch({type:"updateUserRoleFail",payload:error.response.data.message})
+    }
+}
